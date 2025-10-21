fd302c74da74e7021e79f67814b02eee4220dc72
Greeshma <neelapugreeshma2004@gmail.com>
2025-10-22 01:25:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Greeshma Reddy N',
+    author_email='neelapugreeshma2004@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
